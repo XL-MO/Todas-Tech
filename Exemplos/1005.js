05e1c919cd59a1b21133a08640987c800ea0b4b8
Gisely Oliveira <giselymoli@gmail.com>
2023-07-04 21:19:51 -0300

    atividade

--- Exemplos/1005.js
+++
@@ -1,9 +0,0 @@
-var input = require('fs').readFileSync('stdin', 'utf8');
-var lines = input.split('\n');
-function media (){
-    let A = parseFloat(line.shift());
-    let B = parseFloat(line.shift());
-
-    return ((A * 3.5) / 11) + ((B * 7.5) /11);
-}
-console.log(`MEDIA = ${MEDI().tofixed(5)}`);
