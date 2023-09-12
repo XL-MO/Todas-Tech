9f0968bccbf1151e3ee4280923acba3be7b9cf47
Gisely Oliveira <giselymoli@gmail.com>
2023-09-12 14:16:34 -0300

    aula de php

---
+++ Aulas Síncronas/PHP/aula2/desafio1.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Desafio da Aula 02
+ * Criar variáveis para realizar a troca no código (texto entre [])
+ * seguindo o exemplo do nome
+ */
+
+$nomeCliente = 'Alice Rosa';
+$nomeEmpresa = 'Alicelandia';
+$precoProduto = '123.22';
+$dataDeEntrega = "20/05/1985"
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página dinâmica</title>
+</head>
+<body>
+    <h1>Olá <?php echo $nomeCliente; ?></h1>
+    <p>Seja bem vindo ao site: [Nome da empresa]</p>
+    <p>Seu produto [nome do produto] no preço [preço do produto] está sendo preparado e será entrega no dia [data de entrega]</p>
+    <p>Esta compra lhe gerou um cashback de [cash back da compra].
+</body>
+</html>
